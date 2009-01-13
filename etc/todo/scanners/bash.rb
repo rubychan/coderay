3c38cb71a68498951f82c5684cdcf60125a55e1b
murphy <murphy@rubychan.de>
2009-01-13 16:04:44 +0000

    Update for the Bash Scanner (todo).

--- etc/todo/scanners/bash.rb
+++ etc/todo/scanners/bash.rb
@@ -82,7 +82,7 @@ module CodeRay
             elsif match = scan(/[a-zA-Z]\w*/)
               tokens << [match, :ident]
             elsif match = scan(/\#!.*/) # until eof
-              tokens << [match, :preprocessor]
+              tokens << [match, :doctype]
             elsif match = scan(/\#.*/) # until eof  
               tokens << [match, :comment]
             # catch the rest as other

d9ce3ae240d851e1b64ab4d610e46b3f8c8e35d9
murphy <murphy@rubychan.de>
2010-03-26 02:57:00 +0000

    Cleanup of scanner map: - removed :h - it's just a file extension, not a language name - removed :ecma - that's the name of an association, not a language - added :patch as an alias for :diff

--- lib/coderay/scanners/_map.rb
+++ lib/coderay/scanners/_map.rb
@@ -2,10 +2,8 @@ module CodeRay
 module Scanners
 
   map \
-    :h => :c,
     :cplusplus => :cpp,
     :'c++' => :cpp,
-    :ecma => :java_script,
     :ecmascript => :java_script,
     :ecma_script => :java_script,
     :irb => :ruby,
@@ -13,6 +11,7 @@ module Scanners
     :js => :java_script,
     :nitro => :nitro_xhtml,
     :pascal => :delphi,
+    :patch => :diff,
     :plain => :plaintext,
     :xhtml => :html,
     :yml => :yaml

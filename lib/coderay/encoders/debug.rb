bf12a7879244fefa4a56c05166107e9b93333b95
murphy <murphy@rubychan.de>
2010-10-15 22:57:48 +0000

    Cleanup of debug encoder.

--- lib/coderay/encoders/debug.rb
+++ lib/coderay/encoders/debug.rb
@@ -1,6 +1,6 @@
 module CodeRay
 module Encoders
-
+  
   # = Debug Encoder
   #
   # Fast encoder producing simple debug output.
@@ -13,18 +13,16 @@ module Encoders
   # 
   # See also: Scanners::Debug
   class Debug < Encoder
-
+    
     register_for :debug
-
+    
     FILE_EXTENSION = 'raydebug'
     
     def initialize options = {}
       super
       @opened = []
     end
-
-  public
-  
+    
     def text_token text, kind
       if kind == :space
         @out << text
@@ -34,12 +32,12 @@ module Encoders
         @out << kind.to_s << '(' << text << ')'
       end
     end
-
+    
     def begin_group kind
       @opened << kind
       @out << kind.to_s << '<'
     end
-
+    
     def end_group kind
       if @opened.last != kind
         puts @out
@@ -48,16 +46,16 @@ module Encoders
       @opened.pop
       @out << '>'
     end
-
+    
     def begin_line kind
       @out << kind.to_s << '['
     end
-
+    
     def end_line kind
       @out << ']'
     end
-
+    
   end
-
+  
 end
 end

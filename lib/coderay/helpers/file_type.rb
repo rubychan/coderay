eb37e370e921dddcc8a1dcccf37badbc2266c1e0
murphy <murphy@rubychan.de>
2011-02-28 23:33:05 +0000

    Cleanups, fixes and enhancements for the helper classes.

--- lib/coderay/helpers/file_type.rb
+++ lib/coderay/helpers/file_type.rb
@@ -1,69 +1,70 @@
 module CodeRay
-
-# = FileType
-#
-# A simple filetype recognizer.
-#
-# == Usage
-#
-#  # determine the type of the given
-#  lang = FileType[ARGV.first]
-# 
-#  # return :plaintext if the file type is unknown
-#  lang = FileType.fetch ARGV.first, :plaintext
-# 
-#  # try the shebang line, too
-#  lang = FileType.fetch ARGV.first, :plaintext, true
-module FileType
-
-  UnknownFileType = Class.new Exception
-
-  class << self
-
-    # Try to determine the file type of the file.
-    #
-    # +filename+ is a relative or absolute path to a file.
-    #
-    # The file itself is only accessed when +read_shebang+ is set to true.
-    # That means you can get filetypes from files that don't exist.
-    def [] filename, read_shebang = false
-      name = File.basename filename
-      ext = File.extname(name).sub(/^\./, '')  # from last dot, delete the leading dot
-      ext2 = filename.to_s[/\.(.*)/, 1]  # from first dot
-
-      type =
-        TypeFromExt[ext] ||
-        TypeFromExt[ext.downcase] ||
-        (TypeFromExt[ext2] if ext2) ||
-        (TypeFromExt[ext2.downcase] if ext2) ||
-        TypeFromName[name] ||
-        TypeFromName[name.downcase]
-      type ||= shebang(filename) if read_shebang
-
-      type
-    end
-
-    # This works like Hash#fetch.
-    #
-    # If the filetype cannot be found, the +default+ value
-    # is returned.
-    def fetch filename, default = nil, read_shebang = false
-      if default and block_given?
-        warn 'Block supersedes default value argument; use either.'
-      end
-
-      unless type = self[filename, read_shebang]
-        return yield if block_given?
-        return default if default
-        raise UnknownFileType, 'Could not determine type of %p.' % filename
-      end
-      type
-    end
+  
+  # = FileType
+  #
+  # A simple filetype recognizer.
+  #
+  # == Usage
+  #
+  #  # determine the type of the given
+  #  lang = FileType[ARGV.first]
+  #  
+  #  # return :plaintext if the file type is unknown
+  #  lang = FileType.fetch ARGV.first, :plaintext
+  #  
+  #  # try the shebang line, too
+  #  lang = FileType.fetch ARGV.first, :plaintext, true
+  module FileType
     
-  protected
+    UnknownFileType = Class.new Exception
     
-    def shebang filename
-      begin
+    class << self
+      
+      # Try to determine the file type of the file.
+      #
+      # +filename+ is a relative or absolute path to a file.
+      #
+      # The file itself is only accessed when +read_shebang+ is set to true.
+      # That means you can get filetypes from files that don't exist.
+      def [] filename, read_shebang = false
+        name = File.basename filename
+        ext = File.extname(name).sub(/^\./, '')  # from last dot, delete the leading dot
+        ext2 = filename.to_s[/\.(.*)/, 1]  # from first dot
+        
+        type =
+          TypeFromExt[ext] ||
+          TypeFromExt[ext.downcase] ||
+          (TypeFromExt[ext2] if ext2) ||
+          (TypeFromExt[ext2.downcase] if ext2) ||
+          TypeFromName[name] ||
+          TypeFromName[name.downcase]
+        type ||= shebang(filename) if read_shebang
+        
+        type
+      end
+      
+      # This works like Hash#fetch.
+      #
+      # If the filetype cannot be found, the +default+ value
+      # is returned.
+      def fetch filename, default = nil, read_shebang = false
+        if default && block_given?
+          warn 'Block supersedes default value argument; use either.'
+        end
+        
+        if type = self[filename, read_shebang]
+          type
+        else
+          return yield if block_given?
+          return default if default
+          raise UnknownFileType, 'Could not determine type of %p.' % filename
+        end
+      end
+      
+    protected
+      
+      def shebang filename
+        return unless File.exist? filename
         File.open filename, 'r' do |f|
           if first_line = f.gets
             if type = first_line[TypeFromShebang]
@@ -71,67 +72,64 @@ module FileType
             end
           end
         end
-      rescue IOError
-        nil
       end
+      
     end
-
+    
+    TypeFromExt = {
+      'c' => :c,
+      'clj' => :clojure,
+      'css' => :css,
+      'diff' => :diff,
+      'dpr' => :delphi,
+      'gemspec' => :ruby,
+      'groovy' => :groovy,
+      'gvy' => :groovy,
+      'h' => :c,
+      'htm' => :html,
+      'html' => :html,
+      'html.erb' => :rhtml,
+      'java' => :java,
+      'js' => :java_script,
+      'json' => :json,
+      'mab' => :ruby,
+      'pas' => :delphi,
+      'patch' => :diff,
+      'php' => :php,
+      'php3' => :php,
+      'php4' => :php,
+      'php5' => :php,
+      'py' => :python,
+      'py3' => :python,
+      'pyw' => :python,
+      'rake' => :ruby,
+      'raydebug' => :raydebug,
+      'rb' => :ruby,
+      'rbw' => :ruby,
+      'rhtml' => :rhtml,
+      'rjs' => :ruby,
+      'rpdf' => :ruby,
+      'rxml' => :ruby,
+      'sch' => :scheme,
+      'sql' => :sql,
+      'ss' => :scheme,
+      'xhtml' => :xhtml,
+      'xml' => :xml,
+      'yaml' => :yaml,
+      'yml' => :yaml,
+    }
+    for cpp_alias in %w[cc cpp cp cxx c++ C hh hpp h++ cu]
+      TypeFromExt[cpp_alias] = :cpp
+    end
+    
+    TypeFromShebang = /\b(?:ruby|perl|python|sh)\b/
+    
+    TypeFromName = {
+      'Capfile' => :ruby,
+      'Rakefile' => :ruby,
+      'Rantfile' => :ruby,
+    }
+    
   end
-
-  TypeFromExt = {
-    'c' => :c,
-    'clj' => :clojure,
-    'css' => :css,
-    'diff' => :diff,
-    'dpr' => :delphi,
-    'gemspec' => :ruby,
-    'groovy' => :groovy,
-    'gvy' => :groovy,
-    'h' => :c,
-    'htm' => :html,
-    'html' => :html,
-    'html.erb' => :rhtml,
-    'java' => :java,
-    'js' => :java_script,
-    'json' => :json,
-    'mab' => :ruby,
-    'pas' => :delphi,
-    'patch' => :diff,
-    'php' => :php,
-    'php3' => :php,
-    'php4' => :php,
-    'php5' => :php,
-    'py' => :python,
-    'py3' => :python,
-    'pyw' => :python,
-    'rake' => :ruby,
-    'raydebug' => :raydebug,
-    'rb' => :ruby,
-    'rbw' => :ruby,
-    'rhtml' => :rhtml,
-    'rjs' => :ruby,
-    'rpdf' => :ruby,
-    'rxml' => :ruby,
-    'sch' => :scheme,
-    'sql' => :sql,
-    'ss' => :scheme,
-    'xhtml' => :xhtml,
-    'xml' => :xml,
-    'yaml' => :yaml,
-    'yml' => :yaml,
-  }
-  for cpp_alias in %w[cc cpp cp cxx c++ C hh hpp h++ cu]
-    TypeFromExt[cpp_alias] = :cpp
-  end
-
-  TypeFromShebang = /\b(?:ruby|perl|python|sh)\b/
-
-  TypeFromName = {
-    'Capfile' => :ruby,
-    'Rakefile' => :ruby,
-    'Rantfile' => :ruby,
-  }
-
-end
-
+  
 end

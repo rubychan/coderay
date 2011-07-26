74a55182e8422f053cc8efc805c5a077de8f4c8c
Kornelius Kalnbach <murphy@rubychan.de>
2011-07-26 23:08:55 +0200

    add :erb as Ruby scanner alias

--- lib/coderay/scanners/_map.rb
+++ lib/coderay/scanners/_map.rb
@@ -2,20 +2,21 @@ module CodeRay
 module Scanners
   
   map \
-    :cplusplus => :cpp,
-    :'c++' => :cpp,
-    :ecmascript => :java_script,
+    :'c++'       => :cpp,
+    :cplusplus   => :cpp,
+    :ecmascript  => :java_script,
     :ecma_script => :java_script,
-    :irb => :ruby,
-    :javascript => :java_script,
-    :js => :java_script,
-    :nitro => :nitro_xhtml,
-    :pascal => :delphi,
-    :patch => :diff,
-    :plain => :text,
-    :plaintext => :text,
-    :xhtml => :html,
-    :yml => :yaml
+    :erb         => :rhtml,
+    :irb         => :ruby,
+    :javascript  => :java_script,
+    :js          => :java_script,
+    :nitro       => :nitro_xhtml,
+    :pascal      => :delphi,
+    :patch       => :diff,
+    :plain       => :text,
+    :plaintext   => :text,
+    :xhtml       => :html,
+    :yml         => :yaml
   
   default :text
   

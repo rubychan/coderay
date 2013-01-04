6d205f124ab268907fcc950fc4aebff684dfcc4a
Sean Kellogg <skellogg@trilogyinteractive.com>
2013-01-03 18:05:14 -0800

    add new liquid scanner that actually works

--- lib/coderay/scanners/liquid.rb
+++ lib/coderay/scanners/liquid.rb
@@ -3,53 +3,71 @@ module Scanners
 
   load :html
 
+  # Scanner for Handlebars templates.
   class Liquid < Scanner
-   
+
     register_for :liquid
     title 'Liquid Template'
 
     KINDS_NOT_LOC = HTML::KINDS_NOT_LOC
 
-    LIQUID_BLOCK = /
-      ({[{|%])
-      \s
+    LIQUID_OUTPUT_BLOCK = /
+      ({{)
       (.*?)
-      \s
-      ([%|}]})
-    /
-      
-    START_OF_LIQUID = /{{|{%/ 
+      (}})
+    /mx  # :nodoc:
 
-    protected
-   
+    LIQUID_DIRECTIVE_BLOCK = /
+      ({%)
+      (.*?)
+      (%})
+    /mx  # :nodoc:
+
+    START_OF_LIQUID = /{{|{%/  # :nodoc:
+
+  protected
+
     def setup
-      @html_scanner = CodeRay.scanner :html, tokens: @tokens, keep_tokens: true, keep_state: true
-      @liquid_attribute_scanner = CodeRay.scanner :html, tokens: @tokens, keep_tokens: true, keep_stat: true
+      @html_scanner = CodeRay.scanner :html, :tokens => @tokens, :keep_tokens => true, :keep_state => true
+      @liquid_attribute_scanner = CodeRay.scanner :html, :tokens => @tokens, :keep_tokens => true, :keep_state => false
     end
- 
-    def scan_tokens(tokens, options)
+
+    def reset_instance
+      super
+      @html_scanner.reset
+      @liquid_attribute_scanner.reset
+    end
+
+    def scan_tokens encoder, options
       until eos?
         if (match = scan_until(/(?=#{START_OF_LIQUID})/o) || scan_rest) and not match.empty?
-          @html_scanner.tokenize match, tokens: tokens 
-        elsif match = scan(/#{LIQUID_BLOCK}/o)
+          @html_scanner.tokenize match, :tokens => encoder
+
+        elsif match = scan(/#{LIQUID_OUTPUT_BLOCK}/o) || match = scan(/#{LIQUID_DIRECTIVE_BLOCK}/o)
+
           start_tag = self[1]
           code = self[2]
           end_tag = self[3]
-    
-          tokens.begin_group :inline
-          tokens.text_token start_tag, :inline_delimiter
- 
+
+          encoder.begin_group :inline
+          encoder.text_token start_tag, :inline_delimiter
+
           unless code.empty?
-            @liquid_attribute_scanner.tokenize code, tokens: tokens, state: :attribute
+            @liquid_attribute_scanner.tokenize code, :tokens => encoder, :state => :attribute
           end
-          
-          tokens.text_token end_tag, :inline_delimiter unless end_tag.empty?
-          tokens.end_group :inline
-        else 
-          raise_inspect 'else-case reached!', tokens
-        end 
+
+          encoder.text_token end_tag, :inline_delimiter unless end_tag.empty?
+          encoder.end_group :inline
+
+        else
+          raise_inspect 'else-case reached!', encoder
+        end
       end
+      encoder
     end
+
   end
+
 end
 end
+

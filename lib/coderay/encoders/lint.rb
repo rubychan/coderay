dc57601571af8024700991b6a80129285a980e9e
Kornelius Kalnbach <murphy@rubychan.de>
2013-06-30 05:57:38 +0200

    add Lint encoder; do we still need DebugLint?

--- lib/coderay/encoders/lint.rb
+++ lib/coderay/encoders/lint.rb
@@ -1,59 +1,54 @@
 module CodeRay
 module Encoders
   
-  load :lint
-  
-  # = Debug Lint Encoder
+  # = Lint Encoder
   #
-  # Debug encoder with additional checks for:
+  # Checks for:
   # 
   # - empty tokens
   # - incorrect nesting
   # 
   # It will raise an InvalidTokenStream exception when any of the above occurs.
   # 
-  # See also: Encoders::Debug
-  class DebugLint < Debug
+  # See also: Encoders::DebugLint
+  class Lint < Debug
     
-    register_for :debug_lint
+    register_for :lint
+    
+    InvalidTokenStream         = Class.new StandardError
+    EmptyToken                 = Class.new InvalidTokenStream
+    IncorrectTokenGroupNesting = Class.new InvalidTokenStream
     
     def text_token text, kind
-      raise Lint::EmptyToken, 'empty token' if text.empty?
-      super
+      raise EmptyToken, 'empty token' if text.empty?
     end
     
     def begin_group kind
       @opened << kind
-      super
     end
     
     def end_group kind
-      raise Lint::IncorrectTokenGroupNesting, 'We are inside %s, not %p (end_group)' % [@opened.reverse.map(&:inspect).join(' < '), kind] if @opened.last != kind
+      raise IncorrectTokenGroupNesting, 'We are inside %s, not %p (end_group)' % [@opened.reverse.map(&:inspect).join(' < '), kind] if @opened.last != kind
       @opened.pop
-      super
     end
     
     def begin_line kind
       @opened << kind
-      super
     end
     
     def end_line kind
-      raise Lint::IncorrectTokenGroupNesting, 'We are inside %s, not %p (end_line)' % [@opened.reverse.map(&:inspect).join(' < '), kind] if @opened.last != kind
+      raise IncorrectTokenGroupNesting, 'We are inside %s, not %p (end_line)' % [@opened.reverse.map(&:inspect).join(' < '), kind] if @opened.last != kind
       @opened.pop
-      super
     end
     
     protected
     
     def setup options
-      super
       @opened = []
     end
     
     def finish options
       raise 'Some tokens still open at end of token stream: %p' % [@opened] unless @opened.empty?
-      super
     end
     
   end

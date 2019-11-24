8d25b7227f3eb3efb92de2d2ff57e83aed47e8b6
Kornelius Kalnbach <murphy@rubychan.de>
2019-11-24 16:21:29 +0100

    add spec for CodeRay.coderay_path

--- spec/coderay_spec.rb
+++ spec/coderay_spec.rb
@@ -1,9 +1,16 @@
 require File.expand_path('../spec_helper', __FILE__)
 
 RSpec.describe CodeRay do
-  describe 'version' do
+  describe '::VERSION' do
     it "returns the Gem's version" do
       expect(CodeRay::VERSION).to match(/\A\d\.\d\.\d?\z/)
+    end
+  end
+
+  describe '.coderay_path' do
+    it 'returns an absolute file path to the given code file' do
+      base = File.expand_path('../..', __FILE__)
+      expect(CodeRay.coderay_path('file')).to eq("#{base}/lib/coderay/file")
     end
   end
 end

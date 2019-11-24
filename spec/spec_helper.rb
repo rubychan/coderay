77734f6cfa1d90b80c53ac71c880dc5978e58dd7
Kornelius Kalnbach <murphy@rubychan.de>
2019-11-24 16:14:18 +0100

    fix tests for Ruby Enterprise Edition?

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,7 +29,7 @@ RSpec.configure do |config|
     # ...rather than:
     #     # => "be bigger than 2"
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
+  end if RUBY_VERSION >= '1.9'
 
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
